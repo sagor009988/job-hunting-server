6440739bcc78741539c64cc72f54dc9cee6c4d38
Md Saayed <mdsayed.contact@gmail.com>
2023-11-07 20:39:09 +0600

    Added allied api and some bugs fixd

--- index.js
+++ index.js
@@ -50,6 +50,7 @@ async function run() {
         const jobCollection = client.db("jobPortal").collection('jobs');
         const categoriesCollection = client.db("jobPortal").collection('categories');
         const blogCollection = client.db("jobPortal").collection('blogs');
+        const appliedCollection = client.db("jobPortal").collection('applied');
 
 
         app.post('/jobs', async (req, res) => {
@@ -85,10 +86,34 @@ async function run() {
             res.send(result);
         })
 
-        app.get('/blogs', async (req, res) => {
-            const result = await blogCollection.find().toArray();
+        // applied job
+        app.post('/applied', async (req, res) => {
+            const applied = req.body;
+            const result = await appliedCollection.insertOne(applied);
+            res.send(result);
+
+        })
+
+        // applied
+        app.get('/applied', async (req, res) => {
+            const result = await appliedCollection.find().toArray();
             res.send(result);
         })
+
+        // update applide number
+        app.put('/jobs/:id', async (req, res) => {
+            const id = req.params.id;
+            const data = req.body; // You should get the count from the request body
+            const options = { upsert: true };
+            const filter = { _id: new ObjectId(id) };
+            const update = {
+                $set: {
+                    applied: data.newCount // Update the applied count with the provided value
+                }
+            };
+            const result = await jobCollection.updateOne(filter, update, options);
+            res.send(result);
+        });
 
 
         // jwt related api
@@ -112,70 +137,6 @@ async function run() {
             res.clearCookie('token', { maxAge: 0 }).send({ message: 'success' })
         })
 
-
-        // ===========================================================
-
-
-        // // auth api
-        // app.post('/jwt', logger, async (req, res) => {
-        //     const user = req.body;
-        //     const token = jwt.sign(user, process.env.ACCESS_TOKEN_SECRET, { expiresIn: '1h' })
-        //     res
-        //         .cookie('token', token, {
-        //             httpOnly: true,
-        //             secure: true,
-        //             sameSite: 'none'
-        //         })
-        //         .send({ success: true });
-        // })
-
-        // // delete cookie
-        // app.post('/logout', async (req, res) => {
-        //     const user = req.body;
-        //     res.clearCookie('token', { maxAge: 0 }).send({ message: 'success' })
-        // })
-
-        // // services api
-        // app.get('/services', logger, async (req, res) => {
-        //     const cursor = jobCollection.find();
-        //     const result = await cursor.toArray();
-        //     res.send(result);
-        // })
-        // app.get('/services/:id', async (req, res) => {
-        //     const id = req.params.id;
-        //     const query = { _id: new ObjectId(id) }
-        //     const result = await jobCollection.findOne(query);
-        //     res.send(result);
-        // })
-
-        // // bookings
-        // app.post('/bookings', async (req, res) => {
-        //     const bookig = req.body;
-        //     const result = await bookingsCollection.insertOne(bookig);
-        //     res.send(result);
-        // })
-
-        // // get bookings
-        // app.get('/bookings', logger, varifyToken, async (req, res) => {
-        //     // console.log('token from get bookings api:', req.cookies.token);
-        //     if (req.query?.email !== req.user.email) {
-        //         return res.status(403).send({ message: 'Forbidden access.' })
-        //     }
-        //     let query = {}
-        //     if (req.query?.email) {
-        //         query = { email: req.query.email }
-        //     }
-        //     const result = await bookingsCollection.find(query).toArray();
-        //     res.send(result);
-        // })
-
-        // // delete booking
-        // app.delete('/bookings/:id', async (req, res) => {
-        //     const id = req.params.id;
-        //     const query = { _id: new ObjectId(id) }
-        //     const result = bookingsCollection.deleteOne(query);
-        //     res.send(result);
-        // })
 
         await client.db("admin").command({ ping: 1 });
         console.log("Pinged your deployment. You successfully connected to MongoDB!");
